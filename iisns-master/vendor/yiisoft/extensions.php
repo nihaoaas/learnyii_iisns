ac93dd516bde6dea09d9ba38529d9617d6b9e38f
nihaoaas <1159796599@qq.com>
2017-06-12 17:07:55 +0800

    首次

---
+++ iisns-master/vendor/yiisoft/extensions.php
@@ -0,0 +1,105 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.3.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'shiyang/yii2-infinite-scroll' => 
+  array (
+    'name' => 'shiyang/yii2-infinite-scroll',
+    'version' => '1.2.0.0',
+    'alias' => 
+    array (
+      '@shiyang/infinitescroll' => $vendorDir . '/shiyang/yii2-infinite-scroll',
+    ),
+  ),
+  'shiyang/yii2-masonry' => 
+  array (
+    'name' => 'shiyang/yii2-masonry',
+    'version' => '1.0.1.0',
+    'alias' => 
+    array (
+      '@shiyang/masonry' => $vendorDir . '/shiyang/yii2-masonry',
+    ),
+  ),
+  'justinvoelker/yii2-tagging' => 
+  array (
+    'name' => 'justinvoelker/yii2-tagging',
+    'version' => '1.0.2.0',
+    'alias' => 
+    array (
+      '@justinvoelker/tagging' => $vendorDir . '/justinvoelker/yii2-tagging',
+    ),
+  ),
+  'iisns/yii2-webuploader' => 
+  array (
+    'name' => 'iisns/yii2-webuploader',
+    'version' => '1.0.0.0',
+    'alias' => 
+    array (
+      '@iisns/webuploader' => $vendorDir . '/iisns/yii2-webuploader',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'rmrevin/yii2-minify-view' => 
+  array (
+    'name' => 'rmrevin/yii2-minify-view',
+    'version' => '1.13.0.0',
+    'alias' => 
+    array (
+      '@rmrevin/yii/minify' => $vendorDir . '/rmrevin/yii2-minify-view',
+    ),
+  ),
+  'yiisoft/yii2-imagine' => 
+  array (
+    'name' => 'yiisoft/yii2-imagine',
+    'version' => '2.1.0.0',
+    'alias' => 
+    array (
+      '@yii/imagine' => $vendorDir . '/yiisoft/yii2-imagine',
+    ),
+  ),
+);
